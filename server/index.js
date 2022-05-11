711e6b7cf664da52687db0a46147b32b45fb8874
Ben Walpole <btm.walpole@gmail.com>
2022-05-11 11:54:09 +0000

    planning server side stuff

--- server/index.js
+++ server/index.js
@@ -20,6 +20,20 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
   console.log("New socket connected");
 
+  //on connection we will have either: 
+    // A) a sessionID present in localStorage already, so user is auto added to room before entering a name
+    // B) no sessionID found, so we've only connected once the user has entered their username
+    // C) a sessionID present in localStorage for which we have no reference server side
+
+    // We pass the username entered in the front end 
+    //In case B, socket.auth.username is already set client-side, but in A and we need to ensure this is set after the user enters their name
+
+  //on connection, check if there's a sessionID
+    //if present, check if the sessionID has an associated user 
+      //if so, user joins its associated room
+      //if not, tell front end to clear localStorage and assign a new sessionID
+    //if not present, assign one to the user
+
   /*
   socket.on('join', ({name, room}, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
