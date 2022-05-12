d9aaebfd8be9887f2ddf1b13102075c4566fead3
Ben Walpole <btm.walpole@gmail.com>
2022-05-12 16:08:34 +0000

    wip server

--- server/index.js
+++ server/index.js
@@ -17,22 +17,59 @@ const io = new Server(httpServer, {
 
 //app.use(router);
 
+const state = {}; //store the state of the buzzer of each room, and the users in it???? - maybe we can just get this with a function
+const sessions = {}; //allows us to look up room name of a given sessionID
+
 io.on("connection", (socket) => {
   console.log("New socket connected");
+  const sessionID = socket.handshake.auth.sessionID;
 
   //on connection we will have either: 
     // A) a sessionID present in localStorage already, so user is auto added to room before entering a name
     // B) no sessionID found, so we've only connected once the user has entered their username
     // C) a sessionID present in localStorage for which we have no reference server side
 
-    // We pass the username entered in the front end 
-    //In case B, socket.auth.username is already set client-side, but in A and we need to ensure this is set after the user enters their name
+  // We pass the username entered in the front end via the socket.auth object, accessible server-side via socket.handshake.auth
+    // In case A, socket.auth.username is not yet set on the client-side so we need to send that to the client to be set for subsequent requests
+    // In case B, socket.auth.username is already set client-side so we just provide the new session ID
+    // In case C, socket.auth.username is not yet set on the client-side but so we tell client side to clear localStorage and await username to be set
 
   //on connection, check if there's a sessionID
-    //if present, check if the sessionID has an associated user 
-      //if so, user joins its associated room
+    //if present, check if the sessionID has an associated user etc
+      //if so, we set the session details on the socket. user joins its associated room
       //if not, tell front end to clear localStorage and assign a new sessionID
     //if not present, assign one to the user
+
+  if (sessionID) {
+    console.log('sessionID found in localStorage: ', sessionId)
+    // check if we have a reference of this sessionID
+    if (sessions[sessionID]) {
+
+      // setting values on the socket instance
+      socket.sessionID = sessionID;
+      socket.userID = sessions[sessionID].userID;
+      socket.username = sessions[sessionID].username;
+      console.log("found session details: ", sessions[sessionID]);
+
+      socket.emit("oldSession", {
+        userID: socket.userID,
+        roomName: sessions[sessionID].room,
+        oldUserName: socket.username,
+      });
+    } else {
+      console.log(
+        "found sessionID in localStorage " +
+          sessionID +
+          " but no session in server!"
+      );
+      console.log(
+        "telling client to remove session from storage and disconnect"
+      );
+      socket.emit("clearLocalStorage");
+    }
+  } else {
+    console.log('no sessionID found in localStorage')
+  }
 
   /*
   socket.on('join', ({name, room}, callback) => {
