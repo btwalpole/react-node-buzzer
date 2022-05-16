244bcbe98f979d1e6f600662a730456ed0c9e139
Ben Walpole <btm.walpole@gmail.com>
2022-05-16 12:57:25 +0000

    wip trying to connect to backend on gitpod

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000";
+// URL on dev laptop
+// const URL = "http://localhost:5000";
+
+//URL on gitpod
+const URL = "https://5000-btwalpole-reactnodebuzz-sm1gmvmjrdp.ws-eu45.gitpod.io/"
 const socket = io(URL, { autoConnect: false, auth: {} });
 
 socket.onAny((event, ...args) => {
