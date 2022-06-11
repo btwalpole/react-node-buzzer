128c64b8499e19161571979b23c7331a1deef1e6
Ben Walpole <benwalpole95@gmail.com>
2022-06-11 11:18:12 +0100

    wip preparing for deployment to netlify and heroku

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,7 +4,10 @@ import { io } from "socket.io-client";
 //const URL = "http://localhost:5000";
 
 //URL on gitpod
-const URL = "https://5000-btwalpole-reactnodebuzz-sm1gmvmjrdp.ws-eu47.gitpod.io/"
+//const URL = "https://5000-btwalpole-reactnodebuzz-sm1gmvmjrdp.ws-eu47.gitpod.io/"
+
+//URL on heroku
+const URL = "https://awesome-quiz-buzzer.herokuapp.com/";
 const socket = io(URL, { autoConnect: false, auth: {} });
 
 socket.onAny((event, ...args) => {
