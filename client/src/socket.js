41db3223d83ed98f4d630cd75fe6ad70059bf2b7
Ben Walpole <benwalpole95@gmail.com>
2024-03-03 15:53:24 +0000

    updating to connect to render backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,13 +1,13 @@
 import { io } from "socket.io-client";
 
 // URL on dev laptop
-//const URL = "http://localhost:5000";
+// const URL = "http://localhost:5000";
 
 //URL on gitpod
 //const URL = "https://5000-btwalpole-reactnodebuzz-sm1gmvmjrdp.ws-eu47.gitpod.io/"
 
-//URL on heroku
-const URL = "https://awesome-quiz-buzzer.herokuapp.com/";
+//URL on render
+const URL = "https://react-node-buzzer.onrender.com";
 const socket = io(URL, { autoConnect: false, auth: {} });
 
 socket.onAny((event, ...args) => {
